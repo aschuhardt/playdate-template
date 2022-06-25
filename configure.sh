212ef268fe5cdf49d8f4d0f7e670204454a31165
Addison Schuhardt <addison@schuhardt.net>
2022-06-24 22:23:33 -0700

    making configure.sh work

--- configure.sh
+++ configure.sh
@@ -4,6 +4,14 @@ read -e -p "Game name: " TEMPLATE_NAME
 read -e -p "Description: " TEMPLATE_DESC
 read -e -p "Author: " TEMPLATE_AUTHOR
 read -e -p "Build ID: " TEMPLATE_BUILDID
-envsubst < "config/Makefile" > Makefile
-envsubst < "config/CMakeLists.txt" > CMakeLists.txt
-envsubst < "config/Source/pdxinfo" > Source/pdxinfo
+
+export TEMPLATE_NAME=$TEMPLATE_NAME
+export TEMPLATE_DESC=$TEMPLATE_DESC
+export TEMPLATE_AUTHOR=$TEMPLATE_AUTHOR
+export TEMPLATE_BUILDID=$TEMPLATE_BUILDID
+
+TEMPLATE_VARS='$TEMPLATE_NAME:$TEMPLATE_DESC:$TEMPLATE_AUTHOR:$TEMPLATE_BUILDID'
+
+envsubst "$TEMPLATE_VARS" < "config/Makefile" > Makefile
+envsubst "$TEMPLATE_VARS" < "config/CMakeLists.txt" > CMakeLists.txt
+envsubst "$TEMPLATE_VARS" < "config/Source/pdxinfo" > Source/pdxinfo
